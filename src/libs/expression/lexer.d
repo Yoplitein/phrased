1a8b86601362364d26e0693847eb720c5496989d
Steven Dwy <yoplitein+git@gmail.com>
2014-10-17 02:03:06 -0700

    Cleanup lexer

--- src/libs/expression/lexer.d
+++ src/libs/expression/lexer.d
@@ -12,6 +12,8 @@ enum TokenType
     MACRO_END,
 }
 
+private enum EOF = -1;
+
 struct Token
 {
     TokenType type;
@@ -31,27 +33,20 @@ private struct Buffer
     immutable(char) peek(size_t offset = 0)
     {
         if(index + offset >= data.length)
-            return cast(char)-1;
+            return cast(char)EOF;
         else
             return data[index + offset];
     }
     
-    void seek(size_t offset = 1)
+    void advance()
     {
-        index += offset;
+        index++;
     }
     
-    bool empty()
+    bool eof()
     {
         return index >= data.length;
     }
-    
-    /*string toString()
-    {
-        import std.string: format;
-        
-        return "Buffer(\"%s\", %s)".format(data, index);
-    }*/
 }
 
 class LexerError: Exception
@@ -66,7 +61,7 @@ Token[] lex(string expression)
 {
     reset(expression);
     
-    while(!buffer.empty)
+    while(!buffer.eof)
     {
         switch(buffer.peek)
         {
@@ -75,17 +70,14 @@ Token[] lex(string expression)
                 
                 break;
             case '$':
-                //writeln("lex_macro");
                 lex_macro;
                 
                 break;
             case '{':
-                //writeln("lex_choice");
                 lex_choice;
                 
                 break;
             default:
-                //writeln("lex_word");
                 lex_word;
         }
     }
@@ -109,17 +101,17 @@ private int /*charsRead*/ lex_word(bool includeSpace = false)
             push_space;
     }
     
-    loop: while(haveWord && !buffer.empty)
+    loop: while(haveWord && !buffer.eof)
     {
         switch(buffer.peek)
         {
             case '\\':
                 charsRead++;
                 
-                buffer.seek;
+                buffer.advance;
                 
                 goto default;
-            case -1: //EOF
+            case EOF:
             case ' ':
                 if(includeSpace)
                     pushSpace = true;
@@ -159,7 +151,7 @@ private int /*charsRead*/ lex_word(bool includeSpace = false)
                 charsRead++;
         }
         
-        buffer.seek;
+        buffer.advance;
     }
     
     return charsRead;
@@ -167,7 +159,7 @@ private int /*charsRead*/ lex_word(bool includeSpace = false)
 
 private void lex_macro()
 {
-    buffer.seek;
+    buffer.advance;
     
     auto firstChar = buffer.peek;
     bool complex = firstChar == '(';
@@ -176,15 +168,16 @@ private void lex_macro()
     
     if(complex)
     {
+        //[spacing intensifies]
         inMacro = true;
         
-        buffer.seek;
+        buffer.advance;
         
         while(buffer.peek != ')')
             if(lex_word(true) == 0)
                 throw new LexerError("unterminated macro");
         
-        buffer.seek;
+        buffer.advance;
         
         inMacro = false;
     }
@@ -196,7 +189,7 @@ private void lex_macro()
 
 private void lex_choice()
 {
-    buffer.seek;
+    buffer.advance;
     push(TokenType.CHOICE_START);
     
     choiceLevel++;
@@ -207,7 +200,7 @@ private void lex_choice()
         {
             case '\\':
                 goto default;
-            case -1: //probably won't ever happen but just for safety's sake
+            case EOF: //probably won't ever happen but just for safety's sake
                 throw new LexerError("unterminated choice expression");
             case '$':
                 lex_macro;
@@ -215,7 +208,7 @@ private void lex_choice()
                 break;
             case '|':
                 push(TokenType.CHOICE_SEPARATOR);
-                buffer.seek;
+                buffer.advance;
                 
                 goto default;
             case '{':
@@ -223,7 +216,7 @@ private void lex_choice()
                 
                 break;
             case '}':
-                buffer.seek;
+                buffer.advance;
                 
                 break loop;
             default:
@@ -240,7 +233,6 @@ private void lex_choice()
 private void push(TokenType type, string value = "")
 {
     tokens ~= [Token(type, value)];
-    //writefln(`pushing Token(%s, "%s")`, type, value);
 }
 
 private void push_word()
@@ -261,7 +253,7 @@ private void push_space()
     currentWord ~= " ";
     
     push_word;
-    buffer.seek;
+    buffer.advance;
 }
 
 private void reset(string expression)
@@ -273,284 +265,10 @@ private void reset(string expression)
     buffer = Buffer(expression);
 }
 
+//TODO: unittests
+
 private Token[] tokens;
 private string currentWord;
 private bool inMacro;
 private int choiceLevel;
 private Buffer buffer;
-
-/*Token[] lex(string format)
-{
-    Token[] result;
-    string part;
-    bool escape;
-    int choiceLevel;
-    int macroLevel;
-    
-    void add_part()
-    {
-        if(part != "")
-            result ~= [Token(TokenType.WORD, part)];
-        
-        part = null;
-    }
-    
-    foreach(character; format)
-    {
-        switch(character) with(TokenType)
-        {
-            case ' ':
-                add_part();
-                
-                result ~= [Token(TokenType.WORD, " ")];
-                
-                break;
-            case '\\':
-                if(escape)
-                    goto default;
-                
-                escape = true;
-                
-                break;
-            case '{':
-                if(escape)
-                    goto default;
-                
-                add_part();
-                
-                result ~= [Token(CHOICE_START)];
-                choiceLevel++;
-                
-                break;
-            case '}':
-                if(escape || choiceLevel <= 0)
-                    goto default;
-                
-                add_part();
-                
-                result ~= [Token(CHOICE_END)];
-                choiceLevel--;
-                
-                break;
-            case '|':
-                if(escape || choiceLevel > 0 || macroLevel > 0)
-                    goto default;
-                
-                add_part();
-                
-                result ~= [Token(CHOICE_SEPARATOR)];
-                
-                break;
-            case '$':
-                if(escape || macroLevel > 0)
-                    goto default;
-                
-                add_part();
-                
-                macroLevel++;
-                result ~= [Token(MACRO_START)];
-                
-                break;
-            case '(':
-                if(macroLevel > 0)
-                    break;
-                else
-                    goto default;
-            case ')':
-                if(macroLevel <= 0)
-                    goto default;
-                
-                add_part();
-                
-                result ~= [Token(MACRO_END)];
-                macroLevel--;
-                
-                break;
-            default:
-                part ~= character;
-                escape = false;
-        }
-    }
-    
-    if(escape || choiceLevel < 0 || macroLevel < 0)
-        throw new Exception("Unterminated escape, choice or variable expression");
-    
-    add_part();
-    
-    return result;
-}*/
-
-version(none) 
-{
-unittest
-{
-    with(TokenType)
-    {
-        //basic words
-        assert(lex("abc") == [
-            Token(WORD, "abc"),
-        ]);
-        assert(lex("abc 123") == [
-            Token(WORD, "abc"),
-            Token(WORD, " "),
-            Token(WORD, "123"),
-        ]);
-        
-        //choices
-        assert(lex("{abc}") == [
-            Token(CHOICE_START),
-            Token(WORD, "abc"),
-            Token(CHOICE_END),
-        ]);
-        assert(lex("{abc|123}") == [
-            Token(CHOICE_START),
-            Token(WORD, "abc"),
-            Token(CHOICE_SEPARATOR),
-            Token(WORD, "123"),
-            Token(CHOICE_END),
-        ]);
-        assert(lex("{abc 123}") == [
-            Token(CHOICE_START),
-            Token(WORD, "abc"),
-            Token(WORD, " "),
-            Token(WORD, "123"),
-            Token(CHOICE_END),
-        ]);
-        
-        //variables
-        assert(lex("$abc") == [
-            Token(MACRO_START),
-            Token(WORD, "abc"),
-            Token(MACRO_END),
-        ]);
-        assert(lex("$(abc)") == [
-            Token(MACRO_START),
-            Token(WORD, "abc"),
-            Token(MACRO_END),
-        ]);
-        assert(lex("$(abc 123)") == [
-            Token(MACRO_START),
-            Token(WORD, "abc"),
-            Token(WORD, " "),
-            Token(WORD, "123"),
-            Token(MACRO_END),
-        ]);
-        assert(lex("pre$(abc 123)") == [
-            Token(WORD, "pre"),
-            Token(MACRO_START),
-            Token(WORD, "abc"),
-            Token(WORD, " "),
-            Token(WORD, "123"),
-            Token(MACRO_END),
-        ]);
-        assert(lex("$(abc 123)fix") == [
-            Token(MACRO_START),
-            Token(WORD, "abc"),
-            Token(WORD, " "),
-            Token(WORD, "123"),
-            Token(MACRO_END),
-            Token(WORD, "fix"),
-        ]);
-        assert(lex("pre$(abc 123)fix") == [
-            Token(WORD, "pre"),
-            Token(MACRO_START),
-            Token(WORD, "abc"),
-            Token(WORD, " "),
-            Token(WORD, "123"),
-            Token(MACRO_END),
-            Token(WORD, "fix"),
-        ]);
-        assert(lex("pre$((abc 123)fix") == [
-            Token(WORD, "pre"),
-            Token(MACRO_START),
-            Token(WORD, "(abc"),
-            Token(WORD, " "),
-            Token(WORD, "123"),
-            Token(MACRO_END),
-            Token(WORD, "fix"),
-        ]);
-        assert(lex("pre$(abc 123\\))fix") == [
-            Token(WORD, "pre"),
-            Token(MACRO_START),
-            Token(WORD, "abc"),
-            Token(WORD, " "),
-            Token(WORD, "123)"),
-            Token(MACRO_END),
-            Token(WORD, "fix"),
-        ]);
-        
-        //escapes
-        assert(lex("\\{") == [
-            Token(WORD, "{"),
-        ]);
-        assert(lex("\\$") == [
-            Token(WORD, "$"),
-        ]);
-        assert(lex("\\\\") == [
-            Token(WORD, "\\"),
-        ]);
-        assert(lex("\\{abc|123}") == [
-            Token(WORD, "{abc|123}"),
-        ]);
-        assert(lex("{abc\\|123}") == [
-            Token(CHOICE_START),
-            Token(WORD, "abc|123"),
-            Token(CHOICE_END),
-        ]);
-        assert(lex("{abc|123\\}}") == [
-            Token(CHOICE_START),
-            Token(WORD, "abc"),
-            Token(CHOICE_SEPARATOR),
-            Token(WORD, "123}"),
-            Token(CHOICE_END),
-        ]);
-        assert(lex("pre\\$(abc 123)fix") == [
-            Token(WORD, "pre$(abc"),
-            Token(WORD, " "),
-            Token(WORD, "123)fix"),
-        ]);
-        
-        //unterminated expressions
-        try
-        {
-            lex("\\");
-            assert(false);
-        }
-        catch(Exception err) {}
-        
-        try
-        {
-            lex("{");
-            assert(false);
-        }
-        catch(Exception err) {}
-        
-        try
-        {
-            lex("$(");
-            assert(false);
-        }
-        catch(Exception err) {}
-        
-        //variables in choices
-        assert(lex("{$(abc 123)}") == [
-            Token(CHOICE_START),
-            Token(MACRO_START),
-            Token(WORD, "abc"),
-            Token(WORD, " "),
-            Token(WORD, "123"),
-            Token(MACRO_END),
-            Token(CHOICE_END),
-        ]);
-        assert(lex("{$(abc|123)|456}") == [
-            Token(CHOICE_START),
-            Token(MACRO_START),
-            Token(WORD, "abc|123"),
-            Token(MACRO_END),
-            Token(CHOICE_SEPARATOR),
-            Token(WORD, "456"),
-            Token(CHOICE_END),
-        ]);
-    }
-}
-}
