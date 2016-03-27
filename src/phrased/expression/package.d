1db1f8b80b75d11dced5d25742dd229ee3e2b560
Steven Dwy <yoplitein+git@gmail.com>
2016-03-27 01:25:34 -0700

    Simplify phrased.expression.package

--- src/phrased/expression/package.d
+++ src/phrased/expression/package.d
@@ -1,17 +1,7 @@
 /++
-    Package for an implementation of the phrasal template language used within phrased.
-    
-    phrased.expression.lexer and phrased.expression.parser are publically imported.
+    Package for the lexer and parser for the template language. Both are publically imported.
 +/
 module phrased.expression;
 
 public import phrased.expression.lexer;
 public import phrased.expression.parser;
-
-/++
-    Shortcut function to lex, parse and resolve a marked up string.
-+/
-string evaluate(string expression)
-{
-    return expression.lex.parse.resolve;
-}
