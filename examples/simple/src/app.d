a55dd5f8e87adf4158af6a185a5e953fca0aadb5
Steven Dwy <yoplitein+git@gmail.com>
2016-03-30 08:20:18 -0700

    Fix simple example

--- examples/simple/src/app.d
+++ examples/simple/src/app.d
@@ -5,7 +5,8 @@ import phrased;
 void main()
 {
 	auto dictionary = new RuntimeDictionary; //allows for words to be added at runtime
-    defaultDictionary = dictionary; //the macro resolver uses this to look up words
+    Variables vars = default_builtins; //gets a Variables struct with the default builtins preregistered
+    vars.dictionary = dictionary; //sets the dictionary used during evaluation
     
     dictionary.add("object", "world");
     dictionary.add("object", "keyboard");
@@ -13,43 +14,43 @@ void main()
     dictionary.add("adjective", "shiny");
     dictionary.add("adjective", "obtuse");
     dictionary.add("adjective", "purple");
-    register_builtin(
+    vars.register(
         "uniform",
-        (ArgumentRange arguments)
+        (Variables vars, ArgumentRange arguments)
         {
             import std.conv: ConvException, to;
             import std.random: uniform;
             import std.string: format;
             
             if(arguments.length != 3) //the space after the macro name is dropped, but any subsequent spaces are included
-                return builtin_error("expected two arguments, minimum value and maximum value");
+                return vars.error("expected two arguments, minimum value and maximum value");
             
             int min;
             int max;
             
-            //arguments may resolve differently each call
+            //arguments may evaluate differently each call
             //(e.g. if they're made up of choice expressions, further macros, etc.)
             //so their values should be saved if used more than once
-            string minStr = arguments.front.resolve;
+            string minStr = arguments.front.eval(vars);
             
             arguments.popFront; //pop the first argument
             arguments.popFront; //pop the space
             
-            string maxStr = arguments.front.resolve;
+            string maxStr = arguments.front.eval(vars);
             
             try
                 min = minStr.to!int;
             catch(ConvException)
-                return builtin_error(`"%s" is not a number`.format(minStr));
+                return vars.error(`"%s" is not a number`.format(minStr));
             
             
             try
                 max = maxStr.to!int;
             catch(ConvException)
-                return builtin_error(`"%s" is not a number`.format(maxStr));
+                return vars.error(`"%s" is not a number`.format(maxStr));
             
             if(min > max || min == max)
-                return builtin_error("min must be less than max");
+                return vars.error("min must be less than max");
             
             return uniform(min, max).to!string;
         }
@@ -57,5 +58,5 @@ void main()
     
     enum expression = "{hello|greetings|'sup} $(optional $adjective )$(object)! my favorite {number is $(uniform 1 15)|color is {red|blue|green|purple|orange}|language is {D|C|C++}}.";
     
-    writeln(evaluate(expression)); //evaluate lexes, parses and resolves the expression into one of the many possible output strings
+    writeln(expression.evaluate(vars)); //evaluate lexes, parses and evaluates the expression into one of the many possible output strings
 }
